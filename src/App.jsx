6711dac8da059dd6bc868bc839167d0072fa2955
DaifMX <applediegoaif@gmail.com>
2025-05-12 19:06:31 -0600

    First

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { getToken } from './getToken';
+
+import { authFlow, getDataAuth } from './setup';
+
+function App() {
+
+  const handleSetup = async () => {
+    const code = await getDataAuth();
+    authFlow(code)
+  };
+
+  const handleGetToken = () => {
+    getToken();
+  };
+  
+  return (
+    <>
+      <h1>Hola Mund34</h1>
+      <button onClick={handleSetup}>START SETUP </button>
+      <button onClick={handleGetToken}>GET TOKEN </button>
+    </>
+  );
+}
+
+export default App;
